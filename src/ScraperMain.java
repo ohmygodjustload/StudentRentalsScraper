3c3396718131f0d4929bcb9772f3cc2fca6aa9ed
Andrew Peirce <drew101peirce@gmail.com>
2025-11-24 09:24:18 -0600

    Corrected name
    Signed-off-by: Andrew Peirce <drew101peirce@gmail.com>

--- src/ScraperMain.java
+++ src/ScraperMain.java
@@ -10,7 +10,7 @@
 
 
 import java.util.List;
-public class ScaperMain {
+public class ScraperMain {
     public static void main(String[] args) {
         ApartmentScraper scraper = new StudentRentalsScraper(); // Instantiate scraper for StudentRentalsLaCrosse
         List<Apartment> apartments = scraper.scrape();			// Fetch and parse apartment listings
